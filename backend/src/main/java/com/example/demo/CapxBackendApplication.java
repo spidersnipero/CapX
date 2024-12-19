922117b5de7a75190b574ec53908e3376f37068d
spidersipero <ajaykota2003@gmail.com>
2024-12-19 09:19:05 +0530

    configuration

---
+++ backend/src/main/java/com/example/demo/CapxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapxBackendApplication.class, args);
+	}
+
+}
