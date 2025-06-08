13473b9b223c6f671206b097242f2db4db8a4576
AJAY KOTA <96061217+spidersnipero@users.noreply.github.com>
2025-06-08 14:10:22 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Dashboard from "./components/Dashboard/Dashboard";
+
+const DB_URL = "https://13.232.140.204:8443/api/investments";
+
 const App = () => {
   return (
     <div className="dark:bg-black min-h-screen pt-10">
+      <p className="container mx-auto text-sm  lg:text-g dark:text-white">If not able to see the dashboard, please <span className="text-blue-500"><a href={DB_URL}>click here</a></span> and allow the page and come back to website </p>
       <Navbar />
       <Dashboard />
     </div>
